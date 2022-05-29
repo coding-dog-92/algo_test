7c976a4e18b4d5ba5a3efa50872766633f271f4f
agoli <agoli@tencent.com>
2022-05-29 15:21:25 +0800

    f

---
+++ src/com/algo/trees/ValidateBinarySearchTree.java
@@ -0,0 +1,18 @@
+package com.algo.trees;
+
+public class ValidateBinarySearchTree {
+    TreeNode pre = null;
+    public boolean isValidBST(TreeNode root) {
+        if (root==null) {
+            return true;
+        }
+        if (!isValidBST(root.left)) {
+            return false;
+        }
+        if (pre != null && root.val <= pre.val) {
+            return false;
+        }
+        pre = root;
+        return isValidBST(root.right);
+    }
+}
